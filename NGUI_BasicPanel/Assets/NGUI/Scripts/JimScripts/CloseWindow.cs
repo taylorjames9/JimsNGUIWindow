7368717c37733bcfb709e982f268e7eba9f15505
James Taylor <taylorjames9@gmail.com>
2014-04-06 16:12:37 -0500

    THIS IS A SMMOTH WORKING NGUI WINDOW

---
+++ NGUI_BasicPanel/Assets/NGUI/Scripts/JimScripts/CloseWindow.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloseWindow : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+	public void ShutItDown(){
+		gameObject.SetActive (false);
+
+	}
+	public void OpenItUp(){
+		gameObject.SetActive (true);
+
+	}
+
+}
